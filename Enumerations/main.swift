7aac2be9048d185c6610d1c7c60e95ee78816326
Temich93s <nil>
2021-10-01 18:45:02 +1000

    Пройдена тема: Рекурсивные перечисления

--- Enumerations/main.swift
+++ Enumerations/main.swift
@@ -135,3 +135,33 @@ print(myPlanet1)
 let myPlanet2 = Planet4(rawValue: 2)
 print(myPlanet2)
 print(myPlanet2!)
+
+
+//MARK: Рекурсивные перечисления
+print("\n//Рекурсивные перечисления")
+
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+
+print(evaluate(product))
+// Выведет "18"

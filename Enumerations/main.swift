f78cb1be563282d22d0730bdecccf5b9ea1a0f01
Temich93s <nil>
2021-10-01 15:50:41 +1000

    Пройдена тема: Синтаксис перечислений

--- Enumerations/main.swift
+++ Enumerations/main.swift
@@ -9,3 +9,23 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Синтаксис перечислений
+print("\n//Синтаксис перечислений")
+
+enum CompassPoint {
+    case north
+    case east
+    case south
+    case west
+}
+
+enum Planet {
+    case mercury, venus, earth, mars
+}
+
+var direction = CompassPoint.west
+direction = .east
+
+var newPlanet = Planet.mercury
+newPlanet = .earth
